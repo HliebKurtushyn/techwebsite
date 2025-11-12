026758eedb227f9b251a54169cb4039adfa3ba50
HliebKurtushyn <kurtgleb007@gmail.com>
2025-11-12 19:39:55 +0100

    style: move homepage router to this directory

---
+++ app/api/v1/home.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import APIRouter, Request, Response, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordBearer
+from app.api.v1.dependencies import get_current_user
+
+
+load_dotenv()
+
+ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@router.get("/")
+async def home(request: Request, user: tuple | None = Depends(get_current_user, use_cache=False)):
+    flash_msg = request.cookies.get("flash_msg")
+    
+    template_response = templates.TemplateResponse(
+        "home/home.html",
+        {"request": request, "flash_msg": flash_msg, "user": user}
+    )
+    if flash_msg:
+        template_response.delete_cookie("flash_msg")
+    
+    return template_response
+
