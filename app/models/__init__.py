f6d9f5dc8b4fb58e8c323b34bf48ddedf9a7ab69
HliebKurtushyn <kurtgleb007@gmail.com>
2025-11-12 19:41:46 +0100

    style: add '__all__' , which contains all db models

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .problem import Problem
+from .admin_response import AdminResponse
+from .service_record import ServiceRecord
+
+__all__ = ["User", "Problem", "AdminResponse", "ServiceRecord"]
