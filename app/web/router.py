05f8aa907d6fd00380ad1b32aa9ff659e0b07b0b
HliebKurtushyn <kurtgleb007@gmail.com>
2025-11-17 18:24:54 +0100

    refactor(request -> problem):

--- app/web/router.py
+++ app/web/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from app.web.auth.auth import router as auth_router
 from app.web.home.home import router as home_router
-from app.web.requests.requests import router as requests_router
+from app.web.problems.problems import router as problems_router
+from app.web.admin.problems.problems import router as admin_problems_router
 
 router = APIRouter()
 
 router.include_router(auth_router, prefix="/auth")
 router.include_router(home_router)
-router.include_router(requests_router, prefix="/requests")
+router.include_router(problems_router, prefix="/problems")
+router.include_router(admin_problems_router, prefix="/admin/problems")
