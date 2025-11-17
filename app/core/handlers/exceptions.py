860098cd5a6a470279db0ee0cc4e75160310bac0
HliebKurtushyn <kurtgleb007@gmail.com>
2025-11-17 17:16:59 +0100

    feat: add an http error 401 "unauthorized" handler

--- app/core/handlers/exceptions.py
+++ app/core/handlers/exceptions.py
@@ -1,6 +1,6 @@
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
-from starlette.status import HTTP_404_NOT_FOUND, HTTP_403_FORBIDDEN
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_403_FORBIDDEN, HTTP_401_UNAUTHORIZED
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -9,3 +9,6 @@ async def not_found_handler(request: Request, exc):
 
 async def access_denied_handler(request: Request, exc):
     return templates.TemplateResponse("errors/403.html", {"request": request}, status_code=HTTP_403_FORBIDDEN)
+
+async def unauthorized_handler(request: Request, exc):
+    return templates.TemplateResponse("errors/401.html", {"request": request}, status_code=HTTP_401_UNAUTHORIZED)
